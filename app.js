b680d7f405a048864864cbc64e00455e35e2bb85
caleb beam <caleb@LAPTOP-IC1KTCC2>
2023-02-28 17:38:46 -0600

    guess who

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-console.log('howdy partner')
 let myNum = 25
+let guessCount = 0
+let pastNums = ''
+let userName;
+let guessNum;
+
+getName()
+guess();
+//do a new game function
+//create an edge case for
 
 function guess(){
-    let guessNum = prompt('guess a number')
-        if(guessNum < myNum){
-            alert('higher')
-        } else if (guessNum > myNum){
-            alert('lower')
-        } else if (guessNum == myNum){
-            alert('Correct!')
+    getNum()
+        if(guessNum != myNum){
+            if(guessNum > myNum){
+                alert(`Sorry ${userName} guess lower!`)
+                guess()
+            } if(guessNum < myNum){
+                alert(`Sorry ${userName} guess higher!`)
+                guess()
+            } 
+        } else {
+            alert(`Thats correct ${userName}! You only took ${guessCount} tries, your entries were ${pastNums}`)
     }
 }
-guess();
+
+function getName(){
+    userName = prompt('What is your name?')
+}
+
+function getNum(){
+    guessNum = parseInt(prompt('guess a number'))
+    guessCounter()
+}
+
+function guessCounter(){
+    guessCount++
+    pastNums += guessNum + ', '
+}
 
