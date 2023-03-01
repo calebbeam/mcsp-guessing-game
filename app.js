2d7250d69a0dab5a398517433a9a96054615570a
caleb beam <caleb@LAPTOP-IC1KTCC2>
2023-03-01 13:51:36 -0600

    Finished Game

--- app.js
+++ app.js
@@ -3,31 +3,10 @@ let guessCount = 0
 let pastNums = ''
 let userName;
 let guessNum;
+var previousPlayers = {};
 
 getName()
 guess();
-
-function guess(){
-    getNum()
-        if(guessNum != myNum){
-            if(guessNum > myNum){
-                alert(`Sorry ${userName} guess lower!`)
-
-            } if(guessNum < myNum){
-                alert(`Sorry ${userName} guess higher!`)
-
-            } 
-            pastNums += ', '
-            guess()
-        } else if(guessNum == myNum){
-            alert(`Thats correct ${userName}! You only took ${guessCount} tries, your entries were ${pastNums}`)
-            if(prompt(`Would you like to play again ${userName}?`) == 'yes'){
-                playAgain()
-            } else{
-                alert(`Thanks for playing ${userName}`)
-        }
-    }
-}
 
 function getName(){
     userName = prompt('What is your name?')
@@ -43,6 +22,14 @@ function guessCounter(){
     pastNums += guessNum 
 }
 
+function saveUsers(){
+    if(previousPlayers.hasOwnProperty(userName)){
+        return previousPlayers[userName] - guessCount
+    } else {
+        previousPlayers[userName] = guessCount
+    }
+}
+
 function playAgain(){
     guessCount = 0
     pastNums = ''
@@ -51,3 +38,34 @@ function playAgain(){
     getName()
     guess()
 }
+
+function guess(){
+    getNum()
+        if(guessNum != myNum){
+            if(guessNum > myNum){
+                alert(`Sorry ${userName} guess lower!`)
+            } if(guessNum < myNum){
+                alert(`Sorry ${userName} guess higher!`)
+            } 
+            pastNums += ', '
+            guess()
+        } else if(guessNum == myNum){
+            if(saveUsers() > 0){
+                alert(`Thats correct ${userName}! You only took ${guessCount} tries, beating your preivious attempt by ${saveUsers()}, your entries were ${pastNums}`)
+            } else if (saveUsers() < 0){
+                alert(`Thats correct ${userName}! You only took ${guessCount} tries, your preivious attempt was better by ${Math.abs(saveUsers())} attempts, your entries were ${pastNums}`)
+            } else {
+                alert(`Thats correct ${userName}! You only took ${guessCount} tries, your entries were ${pastNums}`)
+            }
+            if(prompt(`Would you like to play again ${userName}?`) == 'yes'){
+                playAgain()
+            } else{
+                alert(`Thanks for playing ${userName}`)
+        }
+    }
+    saveUsers()
+}
+
+
+console.log(previousPlayers['userName'])
+//currently cannot get it to check the value at the userName to compare attemp counts
